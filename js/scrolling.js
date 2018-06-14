31f32745ae24c44cbe7f0be1227bde11a2573cbd
MattDobsonWeb <matt@mattdobson.me>
2018-06-14 18:01:16 +0100

    Images Changed, Dropdown Changed

--- js/scrolling.js
+++ js/scrolling.js
@@ -23,4 +23,11 @@ $(document).ready(function() {
       );
     } // End if
   });
+
+  var navMain = $(".navbar-collapse"); // avoid dependency on #id
+  // "a:not([data-toggle])" - to avoid issues caused
+  // when you have dropdown inside navbar
+  navMain.on("click", "a:not([data-toggle])", null, function() {
+    navMain.collapse("hide");
+  });
 });
